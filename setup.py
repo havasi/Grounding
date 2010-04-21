37139e7a1dc0385fefe1a10c7701b6e48ee7ad94
Catherine Havasi <havasi@new-caledonia.media.mit.edu>
2010-04-21 14:28:20 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+setup(name = 'grounding',
+        version = '0.0',
+        description = 'Learning to ground sensory data using OMCS',
+        author = 'Catherine Havasi and Rob Speer',
+        author_email = 'conceptnet@media.mit.edu',
+        packages = ['./grounding'],
+        package_dir = {'./grounding': 'grounding'},
+        #scripts = ['path/to/script']
+        )
