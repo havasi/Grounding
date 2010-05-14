e8b8188cdb090b1133a3aa775187dfee682948b5
Rob Speer <rspeer@mit.edu>
2010-05-14 13:54:48 -0400

    plotting concept colors in lab color space

--- grounding/color_data.py
+++ grounding/color_data.py
@@ -1,22 +1,40 @@
 from pkg_resources import resource_filename, resource_stream
 from collections import defaultdict
-from csc.conceptnet4.models import RightFeature, Assertion, Language
+from csc.conceptnet.models import RightFeature, Assertion, Language
 from grounding.models import ColorAssertion, NotColorfulAssertion
 from csc.util.persist import PickleDict
 from csc import divisi2
+from colormath.color_objects import LuvColor, LabColor, RGBColor
 from grounding.colorizer import Colorizer
-from grounding import xkcd_plot
 import numpy as np
 import logging
 import random
+
+def rgb_to_luv(rgb):
+    rgbcolor = RGBColor(*rgb)
+    luvcolor = rgbcolor.convert_to('xyz').convert_to('luv')
+    return (luvcolor.luv_l, luvcolor.luv_u, luvcolor.luv_v)
+
+def rgb_to_lab(rgb):
+    rgbcolor = RGBColor(*rgb)
+    labcolor = rgbcolor.convert_to('lab')
+    return (labcolor.lab_l, labcolor.lab_a, labcolor.lab_b)
+
+def lab_to_rgb(lab):
+    labcolor = LabColor(*lab)
+    rgbcolor = labcolor.convert_to('rgb')
+    return (rgbcolor.rgb_r, rgbcolor.rgb_g, rgbcolor.rgb_b)
+
+def luv_to_rgb(luv):
+    luvcolor = LuvColor(*luv)
+    rgbcolor = luvcolor.convert_to('rgb')
+    return (rgbcolor.rgb_r, rgbcolor.rgb_g, rgbcolor.rgb_b)
 
 log = logging.getLogger('colorizer')
 log.setLevel(logging.INFO)
 logging.basicConfig()
 
-pd = PickleDict(resource_filename('grounding', 'pickledata'))
-
-objects_and_colors = defaultdict(list)
+pd = PickleDict(resource_filename('grounding', 'pickledata'), gzip=False)
 
 colorlist = ['blue', 'black', 'brown', 'green', 'grey', 'orange', 'pink', 'purple', 'red', 'white', 'yellow']
 rgb = {'blue': (0,0,255), 'black': (0,0,0), 'brown': (139, 69, 19), 'green': (0, 255, 0), 'grey': (100,100,100), 'orange': (255, 165,0), 'pink': (255,105,180), 'purple': (160, 32, 240), 'red': (255,0,0), 'white': (255, 255, 255), 'yellow': (255,255,0)}
@@ -36,8 +54,126 @@ def medianesque(array):
     distances = [total_distance(array, array[i]) for i in xrange(array.shape[0])]
     return array[np.argmin(distances)]
 
+def component_median(array):
+    return np.median(array, axis=0)
+
+@pd.lazy(version=2)
+def make_luv_color_data():
+    objects_and_colors = defaultdict(list)
+
+    # Nodebox
+    print "Constructing from NodeBox"
+    for color in colorlist:
+        data_stream = resource_stream('grounding', 'data/nodebox/' + color + '.txt')
+        sets = [x.strip('\n') for x in data_stream.readlines()]
+        clist = ','.join(sets)
+        words = clist.split(',')            
+        for word in words:
+            word = word.strip()
+            if word == '': continue
+            print color, word
+            objects_and_colors[en.nl.normalize(word)].append(rgb_to_luv(rgb[color]))
+
+    # ConceptNet
+    print "Constructing from ConceptNet"
+    for color in colorlist:
+        assertions = Assertion.objects.filter(
+            concept2__text=color,
+            relation__name='HasProperty',
+            language__id='en',
+            score__gt=0,
+            frequency__value__gt=0,
+        )
+        for theassertion in assertions:
+            object = theassertion.concept1.text
+            print color, object
+            objects_and_colors[object].append(rgb_to_luv(rgb[color]))
+
+    # Color Doctor
+    print "Constructing from Color Doctor"
+    colorful = ColorAssertion.objects.filter(score__gt=0,)
+    for cd in colorful:
+        object = cd.concept.text
+        ccolor = cd.color
+        color = (ccolor.red, ccolor.green, ccolor.blue)
+        print color, object
+        objects_and_colors[object].append(color)
+
+    # xkcd
+    print "Constructing from xkcd"
+    with open('grouped_color_data.txt') as inputlines:
+        for line in inputlines:
+            try:
+                colorname, userid, r, g, b, monitor, colorblind, male = line.strip().split('|')
+            except ValueError:
+                continue
+            rgblist = [float(r), float(g), float(b)]
+            for concept in en.nl.extract_concepts(colorname, check_conceptnet=True):
+                print rgblist, concept
+                objects_and_colors[concept].append(rgb_to_luv(rgblist))
+
+    return objects_and_colors
+
+@pd.lazy(version=2)
+def make_lab_color_data():
+    objects_and_colors = defaultdict(list)
+
+    # Nodebox
+    print "Constructing from NodeBox"
+    for color in colorlist:
+        data_stream = resource_stream('grounding', 'data/nodebox/' + color + '.txt')
+        sets = [x.strip('\n') for x in data_stream.readlines()]
+        clist = ','.join(sets)
+        words = clist.split(',')            
+        for word in words:
+            word = word.strip()
+            if word == '': continue
+            print color, word
+            objects_and_colors[en.nl.normalize(word)].append(rgb_to_lab(rgb[color]))
+
+    # ConceptNet
+    print "Constructing from ConceptNet"
+    for color in colorlist:
+        assertions = Assertion.objects.filter(
+            concept2__text=color,
+            relation__name='HasProperty',
+            language__id='en',
+            score__gt=0,
+            frequency__value__gt=0,
+        )
+        for theassertion in assertions:
+            object = theassertion.concept1.text
+            print color, object
+            objects_and_colors[object].append(rgb_to_lab(rgb[color]))
+
+    # Color Doctor
+    print "Constructing from Color Doctor"
+    colorful = ColorAssertion.objects.filter(score__gt=0,)
+    for cd in colorful:
+        object = cd.concept.text
+        ccolor = cd.color
+        color = (ccolor.red, ccolor.green, ccolor.blue)
+        print color, object
+        objects_and_colors[object].append(color)
+
+    # xkcd
+    print "Constructing from xkcd"
+    with open('grouped_color_data.txt') as inputlines:
+        for line in inputlines:
+            try:
+                colorname, userid, r, g, b, monitor, colorblind, male = line.strip().split('|')
+            except ValueError:
+                continue
+            rgblist = [float(r), float(g), float(b)]
+            for concept in en.nl.extract_concepts(colorname, check_conceptnet=True):
+                print rgblist, concept
+                objects_and_colors[concept].append(rgb_to_lab(rgblist))
+
+    return objects_and_colors
+
 @pd.lazy(version=2)
 def make_color_data():
+    from grounding import xkcd_plot
     # Nodebox
     print "Constructing from NodeBox"
     for color in colorlist:
@@ -92,9 +228,9 @@ def make_color_data():
 
     return objects_and_colors
 
-@pd.lazy(version=2)
+@pd.lazy(version=3)
 def get_colorfulness():
-    objects_and_colors = make_color_data()
+    objects_and_colors = make_lab_color_data()
     print "Finding Uncolorful Concepts"
     colorfulness = defaultdict(list)
     for object in objects_and_colors:
@@ -106,49 +242,30 @@ def get_colorfulness():
         colorfulness[object].append(0)
     return colorfulness
 
-@pd.lazy(version=3)
+@pd.lazy(version=4)
 def make_color_matrix():
     colorfulness = get_colorfulness()
-    objects_and_colors = make_color_data()
+    objects_and_colors = make_lab_color_data()
     objects = divisi2.OrderedSet()
     objects.extend(colorfulness.keys())
     objects.extend(objects_and_colors.keys())
-    colors = divisi2.DenseMatrix(row_labels=objects, col_labels=['red','green','blue','colorful'])
+    colors = divisi2.DenseMatrix(row_labels=objects, col_labels=['L','a','b','colorful'])
     for thing,values in colorfulness.items():
         colorfulness = np.sum(values)/len(values)
         colors.set_entry_named(thing, 'colorful', colorfulness)
 
     for thing, values in objects_and_colors.items():
-        red, green, blue = medianesque(values)
+        L, a, b = medianesque(np.array(values))
 
-        colors.set_entry_named(thing, 'red', red)
-        colors.set_entry_named(thing, 'green', green)
-        colors.set_entry_named(thing, 'blue', blue)
+        colors.set_entry_named(thing, 'L', L)
+        colors.set_entry_named(thing, 'a', a)
+        colors.set_entry_named(thing, 'b', b)
 
     return colors
 
-def x11_matrix():
-    colormap = {}
-    firstline = True
-    for line in resource_stream('grounding', 'data/x11/rgb.txt'):
-        if firstline: # skip the header
-            firstline = False
-            continue
-        if line.strip():
-            rgb, name = line.strip().split('\t\t')
-            r, g, b = rgb.split()
-            colormap[name] = np.array([r, g, b])
-    colors = divisi2.OrderedSet(colormap.keys())
-    matrix = divisi2.DenseMatrix(row_labels=colors,
-                                 col_labels=['red', 'green', 'blue',
-                                             'colorful'])
-    for colorname, rgb in colormap.items():
-        matrix[matrix.row_index(colorname), 0:3] = rgb
-    matrix[:,3] = 1
-    return matrix
-
 def nearest_color(colormat, rgb):
-    diffs = np.abs(colormat[:,:3] - rgb)
+    lab = rgb_to_lab(rgb)
+    diffs = np.abs(colormat[:,:3] - lab)
     distances = np.sum(diffs, axis=-1)
     best = np.argmin(distances)
     return colormat.row_label(best)
--- grounding/xkcd_plot.py
+++ grounding/xkcd_plot.py
@@ -1,85 +1,55 @@
 from __future__ import with_statement
 import numpy as np
-from matplotlib.colors import rgb_to_hsv
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import cPickle as pickle
 
-def get_plotdata(data_name):
-    colordata = pickle.load(open('data/xkcd/%s' % data_name))
+from color_data import component_median, make_lab_color_data, make_luv_color_data, lab_to_rgb, luv_to_rgb
+
+def get_plotdata():
+    colordata = make_lab_color_data()
     plotdata = {}
-    for key, data in colordata.items():
-        if key.find('nigger') > -1: continue
-        if data['freq'] >= 3 and len(key) >= 3 and data['freq'] * len(key) * len(key) >= 100:
-            rgb = data['rgb'] / 255.0
-            hsv_array = rgb_to_hsv(rgb[np.newaxis, np.newaxis, :])
-            hsv = hsv_array[0,0]
+    for key, colors in colordata.items():
+        if len(colors) > 4:
+            lab = component_median(colors)
+            rgb = [x/255.0 for x in lab_to_rgb(lab)]
             plotdata[key] = {
                 'colorname': key,
                 'rgb': rgb,
-                'hsv': hsv,
-                'freq': data['freq'],
+                'lab': lab,
+                'freq': len(colors)
             }
-            print key, hsv
+            print key, lab
     return plotdata
 
-def make_miniplot():
-    plt.figure(figsize=(10, 10), dpi=100)
-    plt.grid(False)
-    polar = plt.subplot(111, polar=True, axisbg=(0.5, 0.5, 0.5))
-    polar.minorticks_off()
-    polar.set_xticklabels('        ', visible=False)
-    polar.set_yticklabels('        ', visible=False)
-    plotdata = get_plotdata()
-    plotvalues = plotdata.values()
-    plotvalues.sort(key=lambda x: -(x['freq']))
-
-    sizes = np.log([v['freq']-1 for v in plotvalues])/np.log(2)
-    texts = [v['colorname'] for v in plotvalues]
-    hsv = np.array([v['hsv'] for v in plotvalues])
-    rs = hsv[:,1]
-    thetas = (0.25 - hsv[:,0]) * np.pi * 2
-    rgb = np.array([v['rgb'] for v in plotvalues])
-    #rgba = np.zeros( (rgb.shape[0], rgb.shape[1]+1) )
-    #rgba[:,0:3] = rgb
-    #rgba[:,3] = 0.8
-    polar.scatter(thetas, rs, s=sizes*2, c=rgb, edgecolors='none')
-    polar.set_ylim(ymin=0.0, ymax=1.1)
-    
-    plt.savefig('mini_colorwheel.png', dpi=100)
-
-def make_plot(data_name):
+def make_plot():
     plt.figure(figsize=(15, 15), dpi=100)
     plt.grid(False)
-    polar = plt.subplot(111, polar=True, axisbg=(0.5, 0.5, 0.5))
-    polar.minorticks_off()
-    #polar.set_axis_off()
-    polar.set_xticklabels('        ', visible=False)
-    polar.set_yticklabels('        ', visible=False)
+    axes = plt.subplot(111, axisbg=(0.5, 0.5, 0.5))
+    axes.minorticks_off()
     plotdata = get_plotdata()
     plotvalues = plotdata.values()
     plotvalues.sort(key=lambda x: -(x['freq']))
 
     sizes = np.log([v['freq']-1 for v in plotvalues])/np.log(2)
     texts = [v['colorname'] for v in plotvalues]
-    hsv = np.array([v['hsv'] for v in plotvalues])
-    rs = hsv[:,1]
-    thetas = (0.25 - hsv[:,0]) * np.pi * 2
+    lab = np.array([v['lab'] for v in plotvalues])
+    L_components = lab[:,0]
+    a_components = lab[:,1]
+    b_components = lab[:,2]
     rgb = np.array([v['rgb'] for v in plotvalues])
     #rgba = np.zeros( (rgb.shape[0], rgb.shape[1]+1) )
     #rgba[:,0:3] = rgb
     #rgba[:,3] = 0.8
-    polar.scatter(thetas, rs, s=sizes*4, c=rgb, edgecolors='none')
-    polar.set_ylim(ymin=0.0, ymax=1.1)
+    axes.scatter(a_components, b_components, s=sizes*2, c=rgb, edgecolors='none')
     
-    for r, theta, text, size, color in zip(rs, thetas, texts, sizes, rgb):
-        if np.sum(color) > 1.5: textcolor='black'
+    for L, a, b, text, size, color in zip(L_components, a_components, b_components, texts, sizes, rgb):
+        if L >= 50: textcolor='black'
         else: textcolor='white'
         #print text
-        plt.text(theta, r, text.decode('utf-8'), size=size/2.0, color=textcolor)
-    
-    plt.savefig('colorwheel_%s.png' % data_name, dpi=500)
+        plt.text(a, b, text.decode('utf-8'), size=size/2.0, color=textcolor)
+    plt.savefig('concept_colors2.png', dpi=500)
 
 if __name__ == '__main__':
-    make_plot('median_colors')
+    make_plot()
 
