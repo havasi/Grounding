001d7712ec8b413de48eee803387821ed100f90c
Catherine Havasi <havasi@media.mit.edu>
2010-05-19 13:34:14 -0400

    colorizer uses a weighted mean once again

--- grounding/colorizer.py
+++ grounding/colorizer.py
@@ -1,7 +1,7 @@
 from csc import divisi2
 from csc.conceptnet.models import Language, en
 from csc.divisi2.ordered_set import OrderedSet
-from color_data import make_color_matrix, lab_to_rgb, make_user_test, make_test_data, medianesque
+from color_data import make_color_matrix, rgb_to_lab, lab_to_rgb, make_user_test, make_test_data, medianesque, training_and_test_data
 import numpy, math
 import logging
 
@@ -43,27 +43,7 @@ class Colorizer(object):
 
         return divisi2.DenseVector(color, OrderedSet(["L", "a", "b"]))
 
-    def lab_color_for_text(self, text):
-        #if text in self.color_matrix.row_labels:
-        #    return self.color_matrix.row_named(text)
-
-        concepts = en.nl.extract_concepts(text, check_conceptnet=True)
-        if not concepts: return None
-        category = divisi2.SparseVector.from_counts(concepts)
-        vector = self.spreading_activation.left_category(category)
-        aligned_vector = vector[self.concept_label_map]
-        #aligned_vector /= numpy.sum(aligned_vector)
-        #color = divisi2.dot(aligned_vector, self.smaller_color_matrix)
-        print concepts
-        for key, value in aligned_vector.top_items(9):
-            print '\t', key, lab_to_rgb(self.color_matrix.row_named(key))
-        best_colors = [self.color_matrix.row_named(x[0]) for x in aligned_vector.top_items(5)]
-        color = medianesque(best_colors)
-        print '\t', lab_to_rgb(color)
-
-        return divisi2.DenseVector(color, OrderedSet(["L", "a", "b"]))
-
-    def rgb_color_for_text(self, text):
+    def rgb_color_for_concept(self, text):
         l,a,b = self.lab_color_for_concept(text)
         r, g, b = lab_to_rgb((l,a,b))
         return divisi2.DenseVector([r, g, b], OrderedSet(["red", "green", "blue"]))
@@ -85,8 +65,11 @@ def euclid(t1, t2):
     
 def run_leave_n_out():
     from csc.divisi2 import examples
+    import colors
     log.info('Loading ConceptNet matrix')
     sa = examples.spreading_activation()
+    log.info('Loading test input')
+    train_input, test_input = training_and_test_data()
     log.info('Loading test data')
     test = make_test_data()
     log.info('Building colorizer')
@@ -94,6 +77,14 @@ def run_leave_n_out():
     colorizer = Colorizer(sa, cmatrix)
 
     dist_dict = {}
+    baseline_dict = {}
+    distances = {
+        'baseline': [],
+        'weighted': [],
+        'nodebox_prism': [],
+        'inter_annotator': [],
+    }
+
     test_concepts = set(test.keys()) & set(sa.row_labels)
     for colorname in test_concepts:
         try:
@@ -102,13 +93,37 @@ def run_leave_n_out():
             continue
         labact = test[colorname]
         dist = euclid(labout,labact)
-        dist_dict[colorname] = dist
+        
+        distances['weighted'].append(dist)
+
+        baseline = euclid([50, 0, 0], labact)
+        distances['baseline'].append(baseline)
+        
+        try:
+            prismdata = [rgb_to_lab([color.__r, color.__g, color.__b]) for color in colors.prism(colorname)]
+            prism_avg = numpy.mean(numpy.array(prismdata), axis=0)
+            prism_dist = euclid(prism_avg, labact)
+            distances['nodebox_prism'].append(prism_dist)
+        except ZeroDivisionError:
+            pass
+
+        inter_annotator = euclid(rgb_to_lab(test_input[colorname][0]), 
+                                 labact)
+        if inter_annotator == 0:
+            inter_annotator = euclid(rgb_to_lab(test_input[colorname][1]), 
+                                     labact)
+        distances['inter_annotator'].append(inter_annotator)
+        
         rgbact = lab_to_rgb(labact)
         rgbout = lab_to_rgb(labout)
+
         print colorname, rgbact, rgbout, str(dist)
-    total = sum(dist_dict.values())
-    print total, len(dist_dict)
-    return total/len(dist_dict)
+    
+    totals = {}
+    for key, values in distances.items():
+        totals[key] = sum(values)/len(values)
+    print totals
+    return totals
 
 #colorizer = make_colorizer()
 if __name__ == '__main__':
